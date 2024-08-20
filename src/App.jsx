9a99c49f433acd658d0b62b210d52e42fd87f3ce
Himanshu Verma <himanshuverma544@gmail.com>
2024-08-21 00:52:09 +0530

    upd: corrected the code

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,9 @@ import Mcqs from "./components/mcqs/Mcqs";
 
 
 function App() {
-
-  const pages = {
-    ["/mcq-test"]: <Mcqs />,
-  };
-
-  const pagesPath = Object.keys(pages);
-
-  const currentPagePath = window.location.pathname;
-
   
   return (
-    <>
-      {pagesPath.some((pagePath) => currentPagePath.includes(pagePath))
-        ? pages[currentPagePath]
-        : null}
-    </>
+    <Mcqs/>
   );
 }
 
