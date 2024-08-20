a5e0332fc8598adbe5e9be556f3b3d212e338517
Himanshu Verma <himanshuverma544@gmail.com>
2024-08-20 22:10:45 +0530

    upd: created function to show colors as per progress

--- src/components/utils/CircularProgress.jsx
+++ src/components/utils/CircularProgress.jsx
@@ -4,6 +4,24 @@ const CircularProgress = ({ percentage: progress }) => {
   const circumference = 2 * Math.PI * radius;
   const offset = circumference - (progress / 100) * circumference;
 
+  
+  const getColorClass = percentage => {
+
+    if (percentage < 40) {
+      return "#EF4444"; 
+    }
+    else if (percentage >= 40 && percentage < 70) {
+      return "#F59E0B";
+    }
+    else if (percentage >= 70 && percentage < 100) {
+      return "#10B981";
+    }
+    else {
+      return "#3B82F6";
+    }
+  };
+  
+
   return (
     <div className="relative inline-block">
       <svg width="160" height="160" className="-rotate-90">
@@ -18,15 +36,18 @@ const CircularProgress = ({ percentage: progress }) => {
           r={radius}
           cy="80"
           cx="80"
+          stroke={getColorClass(progress)}
           strokeWidth="6"
-          className="fill-transparent stroke-green-500 transition-all"
+          className="fill-transparent transition-all"
           strokeDasharray={circumference}
           strokeDashoffset={offset}
           strokeLinecap="round"
         />
       </svg>
       <div className="flex flex-col justify-center items-center absolute inset-0 text-center font-sans">
-        <div className="text-2xl text-white">{progress}%</div>
+        <div className="text-2xl font-semibold" style={{ color: getColorClass(progress) }}>
+          {`${progress}%`}
+        </div>
       </div>
     </div>
   );
