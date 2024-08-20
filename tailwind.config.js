7f2989cb02aded7846ffb2bc24376c750c96d5a0
Himanshu Verma <himanshuverma544@gmail.com>
2024-08-20 22:09:33 +0530

    upd: added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,14 @@ export default {
     extend: {
       colors: {
         primaryPurple: "#7F00FF",
-        primaryOrange: "#F8931F"
+        secondaryPurple: "#140342",
+
+        primaryOrange: "#F8931F",
+
+        ["light-100"]: "#F9F9F9",
+        ['light-200']: '#F8F4F1',
+        ["light-300"]: "#DFDFDF",
+        ["light-400"]: "#D7D7D7",
       }
     },
   },
