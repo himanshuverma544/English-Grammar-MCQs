3e4c3d1c562f05aa94ad3456649e2ffe9fe55e1e
Himanshu Verma <himanshuverma544@gmail.com>
2024-08-26 19:10:40 +0530

    add: added memoize function

---
+++ src/functions/utils/memoFunc.js
@@ -0,0 +1,18 @@
+export default function memoFunc(func) {
+
+  var cache = {};
+
+  return function (...args) {
+
+    const key = JSON.stringify(args);
+
+    if (key in cache) {
+      return cache[key];
+    }
+    else {
+      const result = func(...args);
+      cache[key] = result;
+      return result;
+    }
+  };
+}
